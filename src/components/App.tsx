c66ab4b6cfd497e89d7f11211812be36cc4bd650
Mauricio Oliveda <maurioliveda@gmail.com>
2024-10-14 21:53:21 -0300

    :construction: Add first components.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+
+import RegisterPage from "../pages/register";
+import LoginPage from "../pages/login";
+import HomePage from "../pages/home";
+import UserContextProvider from "./UserContextProvider";
+import RootLayout from "../pages/layout";
+
+const routes = createBrowserRouter([
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <p>Hello, World!</p>;
+  return (
+    <StrictMode>
+      <UserContextProvider>
+        <RouterProvider router={routes} />
+      </UserContextProvider>
+    </StrictMode>
+  );
 }
