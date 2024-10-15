28a5e340971930f32cb8bcaad5b6a8e101f70e7e
Mauricio Oliveda <maurioliveda@gmail.com>
2024-10-15 01:12:39 -0300

    :construction: Add pages and components related to handle subscriptions.

--- src/components/UserContextProvider.tsx
+++ src/components/UserContextProvider.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import IReactElementProps from "../interfaces/ReactElement";
 import { UserData } from "../types/UserData";
+import { getUserInfo } from "../services/api_service";
 
 export default function UserContextProvider({ children }: IReactElementProps) {
   const [userInfo, setUserInfo] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    getUserInfo().then((value) => {
+      if (value) {
+        setUserInfo(value);
+      }
+    });
+  }, []);
 
   return (
     <UserContext.Provider value={{ userInfo, setUserInfo }}>
